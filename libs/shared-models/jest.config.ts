a791dcda4e608edafe4d90d4ba31fcd9fbc9d33c
ibaicha <ibrahimacamara@gmx.fr>
2025-05-25 15:29:40 +0000

    update commit – 2025-05-25 15:29:40

--- libs/shared-models/jest.config.ts
+++ libs/shared-models/jest.config.ts
@@ -1,6 +1,10 @@
 export default {
   displayName: 'shared-models',
   preset: '../../jest.preset.js',
+  reporters: [
+    'default',
+    process.env['CI'] === 'true' ? 'jest-junit' : null, // Optionnel pour CI
+  ].filter(Boolean),
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
